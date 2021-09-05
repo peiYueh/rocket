5a697d2dca919e697db5ed1cfcf3ed067dd26f63
Peiyueh <peiyueh6663@gmail.com>
2021-09-05 22:44:10 +0800

    i changed the ul & li to div ny

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>祝福语</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off" placeholder="写下你的祝福语" autofocus/>
+            <button>Send</button>
+        </form>
+        <div id='messages'>
+        </div>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('div')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get():
+    return HTMLResponse(html)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(data)
